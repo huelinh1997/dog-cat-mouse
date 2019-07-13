e9c638e9010417c1f8a2d0b9e859b5d1713bc67b
huelinh <nguyenhuelinh.97@gmail.com>
2019-07-13 15:35:03 +0700

    Add sayHi method for Mouse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var myDog = require('./Dog');
 var myCat = require('./cat');
+var myMouse = require('./mouse.js');
 
 var dog = new myDog('Husky');
 var cat = new myCat('Tom');
+var mouse = new myMouse('Jerry');
 // dog.eat(cat);
 // console.log(dog);
 cat.sayHi();
+mouse.sayHi();
--- mouse.js
+++ mouse.js
@@ -1,10 +1,16 @@
-function Mouse() {
+var chalk = require('chalk');
+
+function Mouse(name) {
 	this.name = name;
 	this.isDead = false;
 }
 
 Mouse.prototype.die = function() {
 	this.isDead = true;
+};
+
+Mouse.prototype.sayHi = function() {
+	console.log('I am a mouse, my name is ' + chalk.red(this.name));
 };
 
 module.exports = Mouse;
